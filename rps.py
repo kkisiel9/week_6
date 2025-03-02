81173a60e3c51a2527f5ff414f962f3e7abc050c
Kinga Kisielewska <kkisielewska9@gmail.com>
2025-03-02 15:03:32 +0000

    updates to rps

--- rps.py
+++ rps.py
@@ -3,10 +3,37 @@ import time
 # importing the random module to generate integers for the computer to use as choices
 # importing time to generate dramatic effect before announcing the result
 
-user_to_string_convertion = {"R": "Rock", "P": "Paper", "S": "Scissors"}
-computer_options = {0 : "R", 1: "P", 2: "S"}
+user_input_to_string = {"R": "rock", "P": "paper", "S": "scissors"}
+computer_options_to_letter = {0 : "R", 1: "P", 2: "S"}
 # creating two dictionaries to assign keys and values
-#
+# the first one assigns the value of full string to a letter, the letters being the options a user can enter
+# the second one uses computer options (0,2) and assigns values of letters to them, to match human input to it can be compared later
+
+def convert_comp_choice(comp_choice):
+    """
+this function takes the integer computer generated and converts it into a letter and then a string
+    :param comp_choice: integer choice (0,2). It will then be used in main to compare results
+    :return: full name of computer's choice, str
+    """
+    # generate random in from the range above through importing the random module
+    comp_choice_letter = computer_options_to_letter.get(comp_choice, "")
+    # create a new variable to store obtained from the comp_choice dictionary R,P,S value
+    if comp_choice_letter == "":
+        return ""
+    # default to pass two variables in the get method value, return "" if not found in dict
+    # create a conditional
+    # if "" was passed, the function would also return an empty string to handle the unexpected input
+    return convert_into_string(comp_choice_letter)
+# integer letter returned
+
+def convert_into_string(choice_letter):
+    # this function takes a single letter choice (user input) from the dictionary and converts it into a full name
+    """
+    :param choice_letter: R,P,S from the dictionary
+    :return: the full name of the choice, string
+    """
+    return user_input_to_string.get(choice_letter, "")
+ # Use the user_input_to_string dictionary to get the full name corresponding to the single-letter choice
 
 def result(comp_choice, human_choice):
     """
@@ -22,28 +49,7 @@ def result(comp_choice, human_choice):
         return "win"
     else:
         return "lose"
-
-def convert_into_string(key):
-    # this function takes
-    """
-    :param human_choice:
-    :return:
-    """
-    return user_to_string_convertion.get(key, "")
-
-def convert_comp_choice(comp_choice):
-    """
-
-    :param comp_choice:
-    :return:
-    """
-    # generate random in from the range above through importing the random module
-    # conversion of the computer choice/random integer into a string which corresponds to r/p/s
-    key = computer_options.get(comp_choice, "")
-    if key == "":
-        return ""
-    return convert_into_string(key)
-#
+# multiple conditionals to compare the results and determine winner
 
 def main():
     # main trick implemented here, with the code not forming part of any functions
@@ -52,7 +58,7 @@ def main():
     human_wins = 0
     ties = 0
     computer_wins = 0
-    # scores are zero'd for score tracking purposes before we enter the loop
+    # scores are set to zero for score tracking purposes before we enter the loop
     # create a loop for the game to be able to run multiple times if the user wishes to
     while True:
         # the loop below checks for the user input and executes the function convert_into_string
@@ -62,33 +68,40 @@ def main():
             # create a variable to store the human's choice
             # if passed in lowercase, it'll be converted into upper
             converted_human_choice = convert_into_string(human_choice)
-            # this function takes the human_choice variable collected from the human
-            # it then
+            # create a variable to store the converted into a full word human choice
+            # through creating a function
+            #  which takes the human_choice variable collected from the human (a letter) and converts it into a full word
             if converted_human_choice != "":
                 break
+            #     check if the entered choice is valid, and if so break
             else:
                 print("Please chose a valid value: R,P or S!")
+        #         otherwise, ask the user to input again
 
         comp_choice = random.randrange(0, 3)
+        # generate computer's choice
         converted_comp_choice = convert_comp_choice(comp_choice)
+        # obtain a full name of the choice by using a function
         print(f"You chose {converted_human_choice} and the computer chose {converted_comp_choice}")
         print("Hence...")
         #
         time.sleep(1)
-        # delays the result announcement in the terminal
+        # delays the result announcement in the terminal for dramatic effect
 
         r = result(converted_comp_choice, converted_human_choice)
+        # new variable, r, to store the result obtained from the function called result
         if r == "tie":
             print("It's a tie!")
             ties += 1
+        #     if r equals a tie, then the programme will print the above statement and add a point
         elif r == "win":
             print("YOU WIN!!!")
             human_wins += 1
         else:
             print("YOU LOSE!")
             computer_wins += 1
-        # depending on what the result is, a point is added to the score
-        print(f"Scores: You - {human_wins} point(s), Computer {computer_wins}, Ties - {ties}")
+        print(f"Scores: You - {human_wins} point(s), Computer {computer_wins} point(s), Ties - {ties}")
+        # printing a formatted string to present the results/scores
         question = input("Want to play again? Y/N")
         if question == "N":
             break
